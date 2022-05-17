1e1539e126ed3d183e00f2dd9d285a49dba0d34b
Villegas Ramirez Luis Eduardo <60408295+EduardoVillegas17@users.noreply.github.com>
2022-05-17 00:03:11 -0700

    first-javascript

---
+++ script.js
@@ -0,0 +1,65 @@
+//alert("hello word");
+//prompt("Enter your name: ");
+//console.log("This is a message");
+//console.warn("this is a warning message");
+//console.error("This is an error message");
+
+//Variables
+
+//var x;//declaration of the variable
+//x=10;
+//x=20;//assignation of the value
+//var userName="Eduardo"; //declaration andassignatio
+//console.log(x);
+//console.log(userName);
+
+//let userAge=99;
+//userAge=98;
+//console.log(userAge);
+
+//const taxes=11;
+//console.log(taxes);
+
+//Netflix
+
+let titleMovie="Forrest Gump";
+let titleSerie="SUITES";
+let titleDocumental="Seaspiracy";
+let myList="list of movies";
+let recommended="Movies and Series";
+let actor="Will Smith";
+let director="Guillermo del Toro";
+let genres="Comedy";
+let user="Eduardo";
+let ratings=5;
+let geographicalArea="North America";
+let coincidence= 95;
+let search="Love";
+let description="Forrest Gump is an American comedy-drama film released in 1994. Based on the novel of the same name by writer Winston Groom.";
+let password="mypassword12345";
+let preferences="Action";
+let productionCompanie="Universal Pictures";
+let languaje="Spanish";
+let DirectedBy="Robert B. Weide";
+let runningTime="87 minutes";
+
+document.writeln(titleMovie);
+document.writeln(titleSerie);
+document.writeln(titleDocumental);
+document.writeln(myList);
+document.writeln(recommended);
+document.writeln(actor);
+document.writeln(director);
+document.writeln(genres);
+document.writeln(user);
+document.writeln(ratings);
+document.writeln(geographicalArea);
+document.writeln(coincidence);
+document.writeln(search);
+document.writeln(description);
+document.writeln(password);
+document.writeln(preferences);
+document.writeln(productionCompanie);
+document.writeln(languaje);
+document.writeln(DirectedBy);
+document.writeln(runningTime);
